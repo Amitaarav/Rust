80d44489c58a977a6fc3e0466ba217b376163dbc
Amitaarav <aaravamit55@gmail.com>
2026-04-01 01:56:33 +0530

    added ch-5 struct

---
+++ Rust-book-learn/cha-5/target/debug/deps/cha_5.d
@@ -0,0 +1,5 @@
+C:\Users\USER\Desktop\100xBootcamp\Rust\Rust-book-learn\cha-5\target\debug\deps\cha_5.d: src\main.rs
+
+C:\Users\USER\Desktop\100xBootcamp\Rust\Rust-book-learn\cha-5\target\debug\deps\cha_5.exe: src\main.rs
+
+src\main.rs:
